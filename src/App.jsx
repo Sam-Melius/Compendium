7477ccf196dbe459d6031e132d327cdc01b746f6
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-26 16:55:33 -0700

    wrote test for checking if list is rendering. test is passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { Route, Link, Switch } from 'react-router-dom';
 import List from './views/List';
 
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/list">
-        <List />
-      </Route>
-      <Route path="/">
-        <p>Home</p>
-        <Link to="/list">List</Link>
-      </Route>
-    </Switch>
+    <List />
   );
 }
