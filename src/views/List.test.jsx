39f660541f102beafb60dd15bb13c485f6a1cb86
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-29 11:50:11 -0700

    fixed test for search, search is passing

--- src/views/List.test.jsx
+++ src/views/List.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import List from './List';
 
@@ -11,16 +11,17 @@ describe('List', () => {
         expect(person).toBeInTheDocument();
     })
 
-    // it('tests search function', async () => {
-    //     render(
-    //         <List />
-    //     );
-    //     const search = screen.getByPlaceholderText('Search');
-    //     userEvent.type(search, 'kevin');
+    it('tests search function', async () => {
+        render(
+            <List />
+        );
+        await screen.findByText('Michael Scott')
+        const search = screen.getByPlaceholderText('Search');
+        userEvent.type(search, 'kevin');
 
-    //     return waitFor(() => {
-    //         const result = screen.getByText('Kevin Malone');
-    //         expect(result).toBeInTheDocument();
-    //     })
-    // })
+        return waitFor(() => {
+            const result = screen.getByText('Kevin Malone');
+            expect(result).toBeInTheDocument();
+        })
+    })
 })
